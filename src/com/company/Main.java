7e49c42aa79e2cd510ee4a92482de0e129a8cd19
jisookwon <jinskwon@hotmail.com>
2019-04-19 15:59:43 -0400

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Collections;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+	ArrayList<Double> temperatureList = new ArrayList<Double>();
+	temperatureList.add(40.5);
+	temperatureList.add(33.9);
+	temperatureList.add(37.8);
+	temperatureList.add(15.3);
+	temperatureList.add(25.6);
+	System.out.println(temperatureList);
+
+    }
+}
